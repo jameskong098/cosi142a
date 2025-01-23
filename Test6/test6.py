ae2df5cf8d1cccceb2da17b5b76b0c01f98f55b2
jameskong <jameskong098@gmail.com>
2025-01-23 11:49:18 -0500

    add control light via knob

--- Test6/test6.py
+++ Test6/test6.py
@@ -1,5 +1,15 @@
-from picozero import pico_temp_sensor
+from machine import ADC, Pin, PWM
+import time
 
-temperature = pico_temp_sensor.temp
+adc = ADC(Pin(26))
+# create PWM object from a pin and set the frequency
+pwmO = PWM(Pin(20), freq=2000)
 
-print(temperature)
+try:
+    while True:
+        print(adc.read_u16())
+        # Change the duty cycle of the PWM signal based on the ADC value 
+        pwmO.duty_u16(adc.read_u16())
+        time.sleep(0.2)
+except KeyboardInterrupt:
+    pass
